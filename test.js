3003f271ee369233853db096e4029646841a6e1f
jangseokgyu <43026098+jangseokgyu@users.noreply.github.com>
2019-01-12 04:07:24 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
+var jop = "students";
+var money;
 
+switch(jop){
+  case "teacher":{
+     money = 10000;
+     break;
+   }
+  case "principal":{
+     money = 15000;
+     break;
+   }
+  case "students":{
+     money = 6000;
+     break;
+   }
+   default:{
+     money = 0;
+     break;
+   }
+};
+
+document.writeln( "" );
+document.writeln( "현재금액 : " + money);
