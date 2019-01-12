8e2da4e1b9a6b039e8888624ebb2339862bccd6e
Unknown <jsk053@hanyang.ac.kr>
2019-01-12 16:08:16 +0900

    ver.77p

--- test.js
+++ test.js
@@ -1,47 +1,16 @@
-var jop = "students";
-var money;
+var jpgimg = new Image();
+jpgimg.src = "youdie.jpeg";
+jpgimg.addEventListener("load",drawScreen,false);
 
-switch(jop){
-  case "teacher":{
-     money = 10000;
-     break;
-   }
-  case "principal":{
-     money = 15000;
-     break;
-   }
-  case "students":{
-     money = 6000;
-     break;
-   }
-   default:{
-     money = 0;
-     break;
-   }
-};
-
-document.writeln( "" );
-document.writeln( "현재금액 : " + money);
-
-document.writeln("muliplation theory");
-for(var i =1;i<10;i++){
-  for(var j=1;j<=10;j++) {
-    document.writeln(i+" x "+j+ " = "+ i*j);
-  }
-  document.writeln();
-}
-
-function animal(name){
-  this.name = name;
-}
-
-animal.prototype.talk = function()
+function drawScreen()
 {
-  document.writeln("name is"+this.name);
+  var theCanvas = document.getElementById("GameCanvas");
+  var Context = theCanvas.getContext("2d");
+
+  Context.fillStyle = "#000000";
+  Context.fillRect(0,0,800,600);
+
+  Context.drawImage(jpgimg,0,0);
 }
 
-var money = new animal("원숭이");
-var bird = new animal("새");
-
-money.talk;
-bird.talk;
+drawScreen();
