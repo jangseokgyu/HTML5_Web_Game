2528a40309076cedb81448276083ef30e96e0073
Unknown <jsk053@hanyang.ac.kr>
2019-01-17 03:56:03 +0900

    i don't know why it doesn't work

---
+++ DodgeGame.js
@@ -0,0 +1,21 @@
+window.addEventListener("load",drawScreen,false);
+
+var imgBackground = new Image();
+imgBackground.src = "img/background.jpg";
+imgBackground.addEventListener("load",drawScreen,false);
+
+var imgPlayer = new Image();
+imgPlayer.src = "img/player.jpg";
+imgPlayer.addEventListener("load",drawScreen,false);
+
+function drawScreen()
+{
+  var theCanvas = document.getElementsById("GameCanvas");
+  var Context = theCanvas.getContext("2d");
+
+  Context.fillStyle = "#000000";
+  Context.fillRect(0,0,800,600);
+  Context.drawImage(imgBackground,0,0);
+
+  Context.drawImage(imgPlayer,350,250);
+}
